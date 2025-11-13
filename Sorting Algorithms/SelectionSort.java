6e4b70c9853f50973475b87565393ec0583b1750
Tanzeel Siddique <110907732+TanzeelSid@users.noreply.github.com>
2025-11-13 20:06:15 +0500

    sorting

---
+++ Sorting Algorithms/SelectionSort.java
@@ -0,0 +1,27 @@
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] data = {29, 10, 14, 37, 13};
+        selectionSort(data);
+        System.out.println("Sorted array: ");
+        for (int i : data) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
